5bf47aaf689c7c535b6859a2b8cfb96752a7b6c2
Lucas Emanuel <lucasnobrega.js@gmail.com>
2020-09-08 09:44:54 -0300

    Inserindo a interface de episódios na temporada

---
+++ backend/src/app/interfaces/SeasonEps.ts
@@ -0,0 +1,13 @@
+export interface SeasonEps {
+  seasons_number: number,
+  series_id: number,
+  series_name: String,
+  series_thumbnail: String,
+  series_urlName: String,
+  series_seasonsPath: String,
+  videos_id: number,
+  videos_title: String,
+  videos_number: number,
+  videos_format: String,
+  videos_seasonId: number
+}
