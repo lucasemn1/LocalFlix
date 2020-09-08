95c3408a2fb1528dc0c51bac205f3ecefba40391
Lucas Emanuel <lucasnobrega.js@gmail.com>
2020-09-08 09:46:09 -0300

    Inserindo o login

---
+++ backend/src/util/login.ts
@@ -0,0 +1,22 @@
+import { createConnection } from 'typeorm';
+import { User } from '../app/entity/User';
+
+export async function isUserLogged(username: string) {
+  const connection = await createConnection();
+
+  try{
+    const user = await connection
+    .getRepository(User)
+    .createQueryBuilder('users')
+    .where('users.nickname LIKE :username', { username })
+    .getOne();
+
+    await connection.close()
+    return user ? true: false;
+  }
+  catch(err) {
+    console.log(err);
+    await connection.close();
+    return false;
+  }
+}
