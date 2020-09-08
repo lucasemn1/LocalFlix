afe9d7aea9a45fdde9074c8c3a84ba24ea05985d
Lucas Emanuel <lucasnobrega.js@gmail.com>
2020-09-08 09:45:39 -0300

    Inserindo os repositórios

---
+++ backend/src/app/repository/SeasonRepository.ts
@@ -0,0 +1,31 @@
+import { SeasonEps as ISeasonEps } from '../interfaces/SeasonEps';
+import { createConnection, getConnection, getConnectionManager } from 'typeorm';
+import { Season } from '../entity/Season';
+
+export class SeasonRepository {
+  static async getSeasonEps(urlName: String, season: Number) {
+    const connection = await createConnection();
+
+    try{
+      let seasonEps: Array<ISeasonEps> = await connection
+        .createQueryBuilder()
+        .select('seasons.number')
+        .from(Season, 'seasons')
+        .leftJoinAndSelect('seasons.serie', 'series')
+        .leftJoinAndSelect('seasons.videos', 'videos')
+        .where('series.urlName LIKE :urlName', { urlName })
+        .andWhere('seasons.number = :season', { season })
+        .execute();
+
+      await connection.close();
+      return seasonEps;
+    } 
+    catch(err) {
+      console.log(err);
+      await connection.close();
+      return [];
+    }
+  }
+}
+
+ 
