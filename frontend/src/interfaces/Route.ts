210bd58e429cc4cad405c4050db17e9b0b5316fd
Lucas Nóbrega <lucasnobrega.js@gmail.com.com>
2021-01-22 22:58:28 -0300

    Excluindo o antigo frontend

--- frontend/src/interfaces/Route.ts
+++
@@ -1,5 +0,0 @@
-export interface Route {
-  path: string,
-  name: string,
-  component: () => Object,
-}
--- frontend/src/services/api.ts
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export const baseAPI = 'http://localhost:8000';
-
-export const api = axios.create({ baseURL: baseAPI });
